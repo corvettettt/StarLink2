ab56c5222d1fce9785f0d7db9f115f3cca0cfd82
corvettettt <corvettettt@gmail.com>
2021-03-20 13:46:37 -0500

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import { Header as Haha } from './Header';
+import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <Haha />
+      <Header />
       <Main/>
       <Footer/>
     </div>
